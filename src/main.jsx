f8bba9172c537bab5c3e078221f03f5ca25d4aa0
xLytje <rasmuslytje@gmail.com>
2023-05-23 10:43:59 +0200

    weird fetch

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+var pf = new petfinder.Client({
+  apiKey: `${import.meta.env.VITE_API_KEY}`,
+  secret: `${import.meta.env.VITE_SECRET_KEY}`,
+});
+
+pf.animal
+  .search()
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    // Handle the error
+  });
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />

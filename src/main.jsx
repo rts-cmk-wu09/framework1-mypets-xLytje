d7d895eea6dc23842b397ba76f7b2975de6b37f3
xLytje <rasmuslytje@gmail.com>
2023-05-23 11:22:50 +0200

    bug fixing + axios fetching

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import axios from "axios";
 
-var pf = new petfinder.Client({
-  apiKey: `${import.meta.env.VITE_API_KEY}`,
-  secret: `${import.meta.env.VITE_SECRET_KEY}`,
-});
+// var pf = new petfinder.Client({
+//   apiKey: `${import.meta.env.VITE_API_KEY}`,
+//   secret: `${import.meta.env.VITE_SECRET_KEY}`,
+// });
 
-pf.animal
-  .search()
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    // Handle the error
-  });
+const response = await axios.post(
+  `https://api.petfinder.com/v2/oauth2/token`,
+  `grant_type=client_credentials&client_id=${
+    import.meta.env.VITE_API_KEY
+  }&client_secret=${import.meta.env.VITE_SECRET_KEY}`,
+  {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  }
+);
+
+console.log(response);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
